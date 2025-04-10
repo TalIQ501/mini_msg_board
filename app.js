f6382a96096dc246155353a2db608503dc16eb50
TalIq501 <talhaiqbal501@gmail.com>
2025-04-11 00:52:01 +0500

    Set up EJS

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 const app = express();
 
