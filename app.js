72af9ad8f42a126a935a382ef919c5b2c4da6a66
TalIq501 <talhaiqbal501@gmail.com>
2025-04-11 23:55:52 +0500

    Displaying messages

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+const messages = [
+    {
+        text: "Hi there!",
+        user:  "Amando",
+        added: new Date()
+    },
+    {
+        text: "Helllo, World!",
+        user:  "Charles",
+        added: new Date()
+    }
+]
 
 const app = express();
 
+const PORT = process.env.PORT;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const router = express.Router()
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Mini Message Board", messages })
+})
+
+app.listen(PORT, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening on Port " + PORT)
+    }
+})
